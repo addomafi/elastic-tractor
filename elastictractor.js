b7b0948df3f2c4cd21c067336eaa3766cccc6914
addomafi <adauto.martins@gmail.com>
2018-05-21 12:21:33 -0300

    Fix for lambda function

--- elastictractor.js
+++ elastictractor.js
@@ -6,42 +6,83 @@ let AWS = require('aws-sdk');
 let s3 = new AWS.S3();
 var OnigRegExp = require('oniguruma').OnigRegExp;
 var md5 = require('md5');
+var moment = require('moment');
 // var heapdump = require('heapdump');
 
 var elastictractor = function () {
 	var self = this
+
+	var template = function(tpl, args) {
+		var value = {v: args}
+		var keys = Object.keys(value),
+				fn = new Function(...keys,
+					'return `' + tpl.replace(/`/g, '\\`') + '`');
+		return fn(...keys.map(x => value[x]));
+	};
 
 	self.client = new es.Client({
 		host: process.env.ELK_HOST,
 		log: 'warning'
 	});
 
+	self.matches = function (regex, value) {
+		var onReg = new OnigRegExp(regex);
+		var ret = onReg.testSync(value);
+		onReg = null;
+		return ret;
+	};
+
 	self._init = function() {
 		return new Promise((resolve, reject) => {
 			if (!self.config || process.hrtime(self.config.loadTime)[0] > 60) {
-				s3.getObject({Bucket: 'smiles-devops-configs', Key: 'prd-elasticsearch/patterns.json'}, function(err, data) {
-					if (err) {
-						console.log(err, err.stack);
-						reject(err);
-					} else {
-						console.log("Loaded");
+				self.client.search({
+    			index: ".tractor",
+    			body: {
+            "size": 10000
+          }
+    		}).then(function(body) {
+          var details = [];
+					if (body && body.hits && body.hits.hits.length > 0) {
+						var extractPatterns = _.filter(body.hits.hits, {"_type": "extractor_patterns_v2"})
+
+						var patterns = [];
+						// Create an array of patterns to extract info
+						_.each(extractPatterns, item => {
+							try {
+								patterns.push(extend({
+								}, JSON.parse(item["_source"].patternJSON)))
+							} catch (err) {
+								console.log(`Error to parse a JSON pattern "${item["_source"].patternJSON}"`)
+							}
+						})
+
+						self.config = {
+							loadTime: process.hrtime(),
+							patterns: patterns
+						};
+
 						grok.loadDefault(function (err, patterns) {
 							if (err) reject(err);
 							else {
-								patterns.load("patterns/custom", function (err, newPatterns) {
-									if (err) reject(err);
-									else {
-										self.config = extend({
-											loadTime: process.hrtime()
-										}, JSON.parse(data["Body"].toString()));
-										self.config.grokPatterns = newPatterns
-										resolve(self.config);
+								self.config.grokPatterns = patterns
+								// Load custom Grok patterns
+								var customGrokPatterns = _.sortBy(_.filter(body.hits.hits, {"_type": "patterns"}), [function(o) { return o["_source"].priority; }])
+								_.each(customGrokPatterns, item => {
+									if (!self.config.grokPatterns.getPattern(item["_source"].id)) {
+										self.config.grokPatterns.createPattern(item["_source"].pattern, item["_source"].id)
 									}
-								});
+								})
+								console.log("Loaded");
+								resolve(self.config);
 							}
 						});
+					} else {
+						reject("Extractor patterns wasn't defined, please define it firstly.");
 					}
-				});
+        }, function (error) {
+					console.log(error);
+          reject(error.message);
+        });
 			} else {
 				resolve(self.config);
 			}
@@ -123,7 +164,7 @@ var elastictractor = function () {
     })
 	};
 
-	self._parseRegex = function(regex, data) {
+	self._parseRegex = function(regex, data, timestamp) {
 		return new Promise((resolve, reject) => {
 			var pattern = self.config.grokPatterns.getPattern(md5(regex));
 			if (!pattern) pattern = self.config.grokPatterns.createPattern(regex, md5(regex));
@@ -131,44 +172,140 @@ var elastictractor = function () {
 					if (err) {
 						console.log(err);
 					}
+					// Replace timestamp if data has one
+					if (obj && timestamp) {
+						obj.timestamp = timestamp
+					}
 					resolve(obj);
 			});
 		});
 	}
 
-	self._parse = function(data, config) {
+	/**
+	 * Process the regular expression to extract details
+	 *
+	 * @param  {[type]} data    Document to extract details
+	 * @param  {[type]} pattern Pattern to extract details
+	 * @return {[type]}         Promisse
+	 */
+	self._parse = function(data, pattern) {
 		return new Promise((resolve, reject) => {
+			// Array of promisse in execution
 			var regexInProcessing = [];
-			config.patterns.map(function(regex) {
-				regexInProcessing.push(self._parseRegex(regex, data["_source"][config.fieldName]));
-			})
+			Object.keys(pattern.regexp).forEach( key => {
+				pattern.regexp[key].map(function(regex) {
+					regexInProcessing.push(self._parseRegex(regex, data[key], moment(data.timestamp).format('x')));
+				})
+			});
+			// Wait for all promisses be finished
 			Promise.all(regexInProcessing).then(results => {
+				// Got only valid results
 				var filtered = _.filter(results, x => x);
-				resolve(filtered);
+
+				var additionalExtractor = [];
+				// Keep only data that satisfied all regexp
+				if (filtered.length <  Object.keys(pattern.regexp).length) {
+					filtered = []
+				} else {
+					var newItem = {}
+					filtered.forEach(item => {
+						extend(newItem, item)
+					});
+
+					filtered = [newItem]
+
+					// Do some extra actions if need
+					if (filtered.length && pattern.config.actions) {
+						var onSuccess = pattern.config.actions.onSuccess;
+						// Add info if necessary
+						if (onSuccess) {
+							if (onSuccess.parseJson) {
+								onSuccess.parseJson.map(key => {
+									try {
+										extend(filtered[0], JSON.parse(filtered[0][key]))
+									} catch(e) {
+										// console.log(e)
+									}
+
+									delete filtered[0][key]
+								})
+							}
+							if (onSuccess.add) {
+								Object.keys(onSuccess.add).map(key => {
+									filtered[0][key] = template(onSuccess.add[key], filtered[0])
+								})
+							}
+							if (onSuccess.delete) {
+								onSuccess.delete.map(key => {
+									delete filtered[0][key]
+								})
+							}
+							if (onSuccess.parseInt) {
+								onSuccess.parseInt.map(key => {
+									filtered[0][key] = parseInt(filtered[0][key])
+								})
+							}
+							if (onSuccess.extract) {
+								onSuccess.extract.map(item => {
+									Object.keys(item).map(key => {
+										item[key].map(regexItem => {
+											try {
+												var value = template(`\$\{v.${key}\}`, filtered[0]);
+												additionalExtractor.push({
+													"data": value,
+													"regex": regexItem
+												});
+											} catch (e) {}
+										})
+									})
+								})
+							}
+						}
+					}
+				}
+
+				// Define index name
+				var index;
+				if (filtered.length && filtered[0].hasError) {
+					index = `${pattern.config.index.prefix}error-${moment(data.timestamp, 'x').format('YYYY.MM.DD')}`
+				} else {
+					index = `${pattern.config.index.prefix}${moment(data.timestamp, 'x').format('YYYY.MM.DD')}`
+				}
+
+				// Apply additional extractor if has one
+				if (additionalExtractor.length) {
+					regexInProcessing = [];
+					additionalExtractor.map(item => {
+						regexInProcessing.push(self._parseRegex(item.regex, item.data));
+					})
+					// Wait for all promisses be finished
+					Promise.all(regexInProcessing).then(results => {
+						results.forEach(item => {
+							extend(filtered[0], item)
+						});
+
+						resolve({index: index, type: pattern.config.index.type, id: data["_id"], results: filtered});
+					})
+				} else {
+					resolve({index: index, type: pattern.config.index.type, id: data["_id"], results: filtered});
+				}
 			}).catch(err => {
 				reject(err);
 			});
 		})
 	};
 
-	self._process = function(config, data) {
+	self._processESDocument = function(config, data) {
 		return new Promise((resolve, reject) => {
-			let matches = function (regex, value) {
-				var onReg = new OnigRegExp(regex);
-				var ret = onReg.testSync(value);
-				onReg = null;
-				return ret;
-			};
-
 			if (!self.config) self.config = config;
-			var patterns = _.filter(config.patterns, x => matches(x.component, data["_source"].component));
+			var patterns = _.filter(config.patterns, x => x.config.field && self.matches(x.config.field.regex, data["_source"][config.field.name]));
 			if (patterns.length > 0) {
 				patterns = patterns[0].regex
 			} else {
-				patterns = _.filter(config.patterns, x => x.component === "OTHERS")[0].regex;
+				patterns = _.filter(config.patterns, x => _.indexOf(x.config.source, "OTHERS") > -1)[0].regex;
 			}
 
-			var all = _.filter(config.patterns, x => x.component === "ALL")[0].regex;
+			var all = _.filter(config.patterns, x => _.indexOf(x.config.source, "ALL") > -1)[0].regex;
 			Object.keys(all).forEach(key => {
 				if (patterns[key]) {
 					patterns[key] = all[key].concat(patterns[key])
@@ -241,13 +378,13 @@ var elastictractor = function () {
 		});
 	};
 
-	self._processBacklog = function(index, options) {
+	self._processESDocumentBacklog = function(index, options) {
 		return new Promise((resolve, reject) => {
 			self._getDocumentToReindex(index, options).then(docs => {
 				self._init().then(config => {
 					var docsReindexing = [];
 					Object.keys(docs).forEach(i => {
-						docsReindexing.push(self._process(config, docs[i]));
+						docsReindexing.push(self._processESDocument(config, docs[i]));
 					});
 
 					Promise.all(docsReindexing).then(results => {
@@ -290,12 +427,12 @@ var elastictractor = function () {
 	};
 }
 
-elastictractor.prototype.reindex = function (index, documentId) {
+elastictractor.prototype.reindexESDocument = function (index, documentId) {
 	var self = this
 	return new Promise((resolve, reject) => {
 		self._getDocumentById(index, documentId).then(data => {
 			self._init().then(config => {
-				self._process(config, data).then(docReindexed => {
+				self._processESDocument(config, data).then(docReindexed => {
 					// Update document
 					self.client.update({
 					  index: docReindexed.index,
@@ -322,7 +459,90 @@ elastictractor.prototype.reindex = function (index, documentId) {
 	})
 };
 
-elastictractor.prototype.processBacklog = function (index) {
+/**
+ * Process an event from CloudWatch logs, basically is extract metrics and error from these events
+ *
+ * @param  {[type]} awsLogEvent Event from CloudWatch logs
+ * @return {[type]}             Promise
+ */
+elastictractor.prototype.processAwsLog = function(awsLogEvent) {
+	var self = this
+	return new Promise((resolve, reject) => {
+		// Load configuration
+		self._init().then(config => {
+			// Keep only first pattern related to this CloudWatch logs
+			var pattern = _.head(_.filter(config.patterns, x => x.config && _.indexOf(x.config.source, "aws:awsLogs") > -1 && x.config.field && self.matches(x.config.field.regex, awsLogEvent[x.config.field.name])));
+			// Keep only elegible index
+			pattern.config.index = _.head(_.filter(pattern.config.field.index, x => self.matches(x.regex, awsLogEvent[x.name])))
+
+			var logs = awsLogEvent.logEvents
+			var parsing = [];
+			logs.forEach(data => {
+				data.source = awsLogEvent.logGroup
+				parsing.push(self._parse(data, { config: pattern.config, regexp: pattern.regex}));
+			})
+			// Get results after pattern was applied
+			Promise.all(parsing).then(results => {
+				// Keep only valid data
+				results = _.filter(results, x => x.results.length)
+
+				var body = [];
+				// Contructs the body object to foward to elasticsearch
+				results.forEach(function(item) {
+					body.push({ index:  { _index: item.index, _type: "metric" } });
+					body.push(item);
+				});
+
+				//  Send documents to elasticsearch, if has one
+				if (body.length > 0) {
+					self.client.bulk({
+						body: body
+					}, function (error, response) {
+						if (error) {
+							console.log(error);
+							reject(error);
+						} else {
+							resolve(response);
+						}
+					});
+				}
+
+				parsing = null;
+				all = null;
+				patterns = null;
+			}).catch(err => {
+				reject(err);
+				parsing = null;
+				all = null;
+				patterns = null;
+			});
+		});
+	})
+};
+
+
+elastictractor.prototype.processS3 = function(s3Event) {
+	var self = this
+	return new Promise((resolve, reject) => {
+
+	});
+};
+
+elastictractor.prototype.processSNS = function(snsEvent) {
+	var self = this
+	return new Promise((resolve, reject) => {
+		tractor.reindexESDocument(component.Sns.Subject, component.Sns.Message).then(response => {
+			resolve(response);
+		}).catch(err => {
+			reject(err);
+		});
+	});
+};
+
+/*
+* Process any elasticsearch document that contains a text "error" on the field errorMessage.
+*/
+elastictractor.prototype.processESBacklog = function (index) {
 	var self = this
 	return new Promise((resolve, reject) => {
 		var minTimestamp = 0;
@@ -338,7 +558,7 @@ elastictractor.prototype.processBacklog = function (index) {
 						maxTimestamp: maxTimestamp
 					};
 					var overloadCall = function(index, options) {
-						self._processBacklog(index, options).then(results => {
+						self._processESDocumentBacklog(index, options).then(results => {
 							if (!(options.minTimestamp < options.maxTimestamp)) {
 								console.log('Finish');
 								resolve(results);
