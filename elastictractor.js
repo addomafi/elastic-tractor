68fe97f0ebf430689abeae3f5229c0ec754aa124
addomafi <adauto.martins@gmail.com>
2018-08-04 10:45:32 -0300

    fix

--- elastictractor.js
+++ elastictractor.js
@@ -478,29 +478,31 @@ var elastictractor = function () {
 		return new Promise((resolve, reject) => {
 			var patternsInProcessing = []
 			config.patterns.map(pattern => {
-				patternsInProcessing.push(new Promise((resolve, reject) => {
-					// Keep only elegible index
-					pattern.config.output = _.filter(pattern.config.field.output, x => self.matches(x.regex, template(pattern.config.field.name, event)))
+				patternsInProcessing.push(PromiseBB.map(chunks, function(chunk) {
+						return new Promise((resolve, reject) => {
+						// Keep only elegible index
+						pattern.config.output = _.filter(pattern.config.field.output, x => self.matches(x.regex, template(pattern.config.field.name, event)))
 
-					var logs = event[pattern.config.field.data];
-					delete event[pattern.config.field.data];
-					var parsing = [];
-					logs.forEach(data => {
-						data.source = template(pattern.config.field.name, event);
-						parsing.push(self._parse(data, { config: pattern.config, regexp: pattern.regex }));
+						var logs = event[pattern.config.field.data];
+						delete event[pattern.config.field.data];
+						var parsing = [];
+						logs.forEach(data => {
+							data.source = template(pattern.config.field.name, event);
+							parsing.push(self._parse(data, { config: pattern.config, regexp: pattern.regex }));
+						})
+						logs = [];
+						// Get results after pattern was applied
+						Promise.all(parsing).then(results => {
+							// Keep only valid data
+							results = _.filter(results, x => x.results.length)
+							resolve(results);
+							parsing = null;
+						}).catch(err => {
+							reject(err);
+							parsing = null;
+						});
 					})
-					logs = [];
-					// Get results after pattern was applied
-					Promise.all(parsing).then(results => {
-						// Keep only valid data
-						results = _.filter(results, x => x.results.length)
-						resolve(results);
-						parsing = null;
-					}).catch(err => {
-						reject(err);
-						parsing = null;
-					});
-				}))
+				}, {concurrency: 100})
 			})
 
 			Promise.all(patternsInProcessing).then(results => {
